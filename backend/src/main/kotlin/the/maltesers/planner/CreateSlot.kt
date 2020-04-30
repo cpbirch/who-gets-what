0589a7510577c0e4aa115d81e358c14e1bfdb1ce
Albert Attard <albert.attard@thoughtworks.com>
2020-04-30 12:53:21 +0200

    Minor changes

--- backend/src/main/kotlin/the/maltesers/planner/CreateSlot.kt
+++ backend/src/main/kotlin/the/maltesers/planner/CreateSlot.kt
@@ -6,6 +6,6 @@ import java.util.Locale
 
 data class CreateSlot(val date: LocalDate, val ppeType: String, val state: SlotState) {
 
-  val year = date.year
+  val year = date.get(WeekFields.of(Locale.getDefault()).weekBasedYear())
   val week = date.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear())
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,32 @@
 allprojects {
-    repositories {
-        mavenLocal()
-        jcenter()
-    }
+  repositories {
+    mavenLocal()
+    jcenter()
+  }
 }
 
 subprojects {
-    version = "1.0"
+  version = "1.0"
 }
 
 task("copyFrontendResources") {
-    group = "build"
-    description = "Copy frontend resources into backend"
-    dependsOn(":frontend:build")
+  group = "build"
+  description = "Copy frontend resources into backend"
+  dependsOn(":frontend:build")
 
-    doFirst {
-        copy {
-            from("./frontend/dist")
-            into("./backend/generated/main/resources/web")
-        }
+  doFirst {
+    delete("./backend/generated/main/resources/web")
+    copy {
+      from("./frontend/dist")
+      into("./backend/generated/main/resources/web")
     }
+  }
 }
 
 task("assembleAll") {
-    group = "build"
-    description = "Build combined backend & frontend into one JAR"
-    dependsOn("copyFrontendResources", ":backend:shadowJar")
+  group = "build"
+  description = "Build combined backend & frontend into one JAR"
+  dependsOn("copyFrontendResources", ":backend:shadowJar")
 }
 
 defaultTasks("clean", "ktlintFormat", "dependencyUpdates", "test")
